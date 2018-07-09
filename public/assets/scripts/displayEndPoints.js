7c6a1afb3dc063b28a27d912a16481fc29f57a9e
jermbo@cosmicstrawberry.com <jermbo@cosmicstrawberry.com>
2018-07-08 21:01:18 -0400

    GOING IN A COMPLETELY DIFFERENT DIRECTION

---
+++ public/assets/scripts/displayEndPoints.js
@@ -0,0 +1,18 @@
+const DisplayEndPoints = function(opts) {
+  const baseURL = `${window.location.protocol}//${window.location.host}`;
+  const endpointDisplay = document.querySelector(opts.elem);
+  fetch(`${baseURL}/${opts.db}/${opts.db}.json`)
+    .then(resp => resp.json())
+    .then(json => getRoutes(json));
+
+  function getRoutes(data) {
+    const keys = Object.keys(data);
+
+    endpointDisplay.innerHTML = keys
+      .map(
+        key =>
+          `<a href="${baseURL}/futurama/${key}" class="endpoint" target="_blank">${key}</a>`
+      )
+      .join("");
+  }
+};
