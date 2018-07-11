c866e32ab02802b7d552c8429fc418339dc60c57
jermbo <jermbo@cosmicstrawberry.com>
2018-07-11 16:28:58 -0400

    adding home page styles

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const jsonServer = require("json-server");
 const express = require("express");
 const path = require("path");
 const server = jsonServer.create();
-const futurama = jsonServer.router("./futurama/futurama.json");
 const middleware = jsonServer.defaults();
 const port = process.env.PORT || 5000;
 
@@ -11,7 +10,6 @@ server.use(
   "/scripts",
   express.static(path.join(__dirname, "./public/scripts/"))
 );
-// express.static.mime.define({'text/plain': ['md']});
 
 server.use("/futurama", express.static(path.join(__dirname, "futurama")));
 server.use("/futurama", jsonServer.router("./futurama/futurama.json"));
