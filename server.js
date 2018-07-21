02efa58a08b2bb0417dcc922b7194fa8102f20de
jermbo@cosmicstrawberry.com <jermbo@cosmicstrawberry.com>
2018-07-21 19:45:35 -0400

    starting some fakebank data

--- server.js
+++ server.js
@@ -4,16 +4,22 @@ const path = require("path");
 const server = jsonServer.create();
 const middleware = jsonServer.defaults();
 const port = process.env.PORT || 5000;
-const pages = ["futurama", "avatar", "baseball"];
-const fs = require('fs');  
+const pages = ["futurama", "avatar", "baseball", "fakebank"];
+const fs = require("fs");
 
-server.get("/reset", (req,res) => {
-    pages.forEach(page => {
-        fs.copyFile(`./${page}/${page}.json.backup`, `./${page}/${page}.json`, (err) => {
-  	  if (err) { console.error(err); } 
-	});
-    });
-    res.end("ok");
+server.get("/reset", (req, res) => {
+  pages.forEach(page => {
+    fs.copyFile(
+      `./${page}/${page}.json.backup`,
+      `./${page}/${page}.json`,
+      err => {
+        if (err) {
+          console.error(err);
+        }
+      }
+    );
+  });
+  res.end("ok");
 });
 
 server.use(middleware);
