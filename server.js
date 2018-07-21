210467faf992552c3c695a8a3aa9a30969410d32
jermbo@cosmicstrawberry.com <jermbo@cosmicstrawberry.com>
2018-07-21 19:35:40 -0400

    starting recipes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const server = jsonServer.create();
 const middleware = jsonServer.defaults();
 const port = process.env.PORT || 5000;
-const pages = ["futurama", "avatar", "baseball"];
+const pages = ["futurama", "avatar", "baseball", "recipes"];
 
 server.use(middleware);
 
