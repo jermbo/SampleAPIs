44f148a5edf6428dff0977d9bdb0c6736d2a1339
jermbo <jermbo@cosmicstrawberry.com>
2018-10-06 17:11:01 -0400

    updating presidents adding the call to the home page correcting one entry

--- server.js
+++ server.js
@@ -5,27 +5,15 @@ const fs = require("fs");
 const server = jsonServer.create();
 const middleware = jsonServer.defaults();
 const port = process.env.PORT || 5000;
-const pages = [
-  "futurama",
-  "avatar",
-  "baseball",
-  "recipes",
-  "fakebank",
-  "football",
-  "countries"
-];
+const pages = ["futurama", "avatar", "baseball", "recipes", "fakebank", "football", "countries", "presidents"];
 
 server.get("/reset", (req, res) => {
   pages.forEach(page => {
-    fs.copyFile(
-      `./${page}/${page}.json.backup`,
-      `./${page}/${page}.json`,
-      err => {
-        if (err) {
-          console.error(err);
-        }
+    fs.copyFile(`./${page}/${page}.json.backup`, `./${page}/${page}.json`, err => {
+      if (err) {
+        console.error(err);
       }
-    );
+    });
   });
   res.end("ok");
 });
