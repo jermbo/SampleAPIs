9d5aaa590fcdb45a375728e752e07e310f2551ac
Jermbo <jermbo@cosmicstrawberry.com>
2020-01-08 15:52:38 -0500

    setting up a better way to expand endpoint pages

---
+++ src/apiList.js
@@ -0,0 +1,105 @@
+module.exports = [
+  {
+    id: 1,
+    title: "Futurama",
+    desc:
+      "An API with characters, episode listing, species, planets, and trivia questions.",
+    link: "futurama"
+  },
+  {
+    id: 2,
+    title: "Avatar",
+    desc: "An API with characters, episode listings, and trivia questions.",
+    link: "avatar"
+  },
+  {
+    id: 3,
+    title: "Baseball",
+    desc: "An API with records and trivia questions.",
+    link: "baseball"
+  },
+  {
+    id: 4,
+    title: "Recipes",
+    desc: "A recipe database",
+    link: "recipes"
+  },
+  {
+    id: 5,
+    title: "FakeBank",
+    desc: "Just a random set of fake bank data.",
+    link: "fakebank"
+  },
+  {
+    id: 6,
+    title: "Football",
+    desc: "An API with records and trivia questions.",
+    link: "football"
+  },
+  {
+    id: 7,
+    title: "Countries",
+    desc: "An API with information about countries.",
+    link: "countries"
+  },
+  {
+    id: 8,
+    title: "Presidents",
+    desc: "Millions of peaches! No...not those Presidents...",
+    link: "presidents"
+  },
+  {
+    id: 9,
+    title: "Simpsons",
+    desc: "Because who doesn't need easily accessible data about the simpsons?",
+    link: "simpsons"
+  },
+  {
+    id: 10,
+    title: "Hurricanes",
+    desc: "Hurricanes",
+    link: "hurricanes"
+  },
+  {
+    id: 11,
+    title: "Movies",
+    desc: "Movies",
+    link: "movies"
+  },
+  {
+    id: 12,
+    title: "Wines",
+    desc: "Wines",
+    link: "wines"
+  },
+  {
+    id: 13,
+    title: "Health",
+    desc: "An API with health and medical information",
+    link: "health"
+  },
+  {
+    id: 13,
+    title: "Beers",
+    desc: "Beers",
+    link: "beers"
+  },
+  {
+    id: 14,
+    title: "Switch Games",
+    desc: "Figured it would be fun to have a Switch game list on here.",
+    link: "switch"
+  },
+  {
+    id: 15,
+    title: "PlayStation Games",
+    desc: "Figured it would be fun to have a PlayStation game list on here.",
+    link: "playstation"
+  },
+  {
+    id: 16,
+    title: "XBox Games",
+    desc: "Figured it would be fun to have a Xbox game list on here.",
+    link: "xbox"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -3,25 +3,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const port = process.env.PORT || 5001;
-const pages = [
-  "futurama",
-  "avatar",
-  "baseball",
-  "recipes",
-  "fakebank",
-  "football",
-  "countries",
-  "presidents",
-  "simpsons",
-  "hurricanes",
-  "movies",
-  "wines",
-  "health",
-  "beers",
-  "switch",
-  "playstation",
-  "xbox"
-];
+const ApiList = require("./apiList");
 
 const app = express();
 
@@ -41,19 +23,35 @@ app.get("/api-reset", (req, res) => {
   res.send("ok");
 });
 
+app.set("view engine", "pug");
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.render("index", {
+    apiList: JSON.stringify(ApiList)
+  });
+});
+
+app.get("/:id", (req, res) => {
+  const id = req.params.id.toLowerCase();
+  const data = ApiList.filter(api => id == api.title.toLowerCase())[0];
+
+  if (data) {
+    res.render("page", {
+      ...data
+    });
+  } else {
+    res.render("404");
+  }
 });
 
 app.use(express.static(path.join(__dirname, "/public")));
 
-pages.forEach(page => {
-  app.use(`/${page}`, express.static(path.join(__dirname, `/api/${page}`)));
-  app.use(
-    `/${page}/api`,
-    jsonServer.router(path.join(__dirname, `/api/${page}/${page}.json`))
-  );
-});
+// pages.forEach(page => {
+//   app.use(`/${page}`, express.static(path.join(__dirname, `/api/${page}`)));
+//   app.use(
+//     `/${page}/api`,
+//     jsonServer.router(path.join(__dirname, `/api/${page}/${page}.json`))
+//   );
+// });
 
 app.listen(port, () => {
   console.log(`Express is now : http://localhost:${port}`);
