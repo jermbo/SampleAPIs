5aa6f0bca70c7118ee9c034015fdb2adeef3bc8d
Jermbo <jermbo@cosmicstrawberry.com>
2020-10-08 16:33:01 -0400

    📦 NEW: adding ability to create new files
    This is the foundation for creating new custom endpoints.
    Need to create a custom landing page for users to create.
    Maybe create a page to list existing endpoints, expirations, etc.
    But this is another issue all together.

--- src/routes/custom-apis.js
+++ src/routes/custom-apis.js
@@ -1,36 +1,98 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const rateLimit = require("express-rate-limit");
 const jsonServer = require("json-server");
-// const jsonGraphqlExpress = require("json-graphql-server");
-// const { getFromFile } = require("../utils");
+const jsonGraphqlExpress = require("json-graphql-server");
+const { getFromFile } = require("../utils");
 
+const { apiLimits } = require("../utils/rateLimiterDefaults");
 const router = express.Router();
 
-const apiLimiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 500,
-  message: "Too many requests, please try again after five minutes."
-});
-
-// router.use(
-//   '/:id',
-//   apiLimiter,
-//   // jsonServer.router(path.join(__dirname, '../custom/jermbo.json'))
-//   jsonServer.router(path.join(__dirname, `../custom/jermbo.json`))
-// );
-
-
-let customFiles = [];
+let CustomEndpoints = [];
 const directoryPath = path.join(__dirname, "../custom");
-async function init() {
-  console.log('getting files')
-  customFiles = await fs.readdirSync(directoryPath);
-  console.log(customFiles)
+
+const init = async () =>  {
+  CustomEndpoints = [];
+  await createFileMetaData();
+  registerCustomLandingPages();
+  registerCustomEndPoints();
 }
+
+const createFileMetaData = async () => {
+  const files = await fs.readdirSync(directoryPath);
+  CustomEndpoints = files.map((file) => {
+    const data = JSON.parse(fs.readFileSync(path.join(__dirname, `../custom/${file}`)));
+    const endPoints = Object.keys(data);
+    const name = file.split('.')[0].toLowerCase();
+    
+    return {
+      name,
+      link: name,
+      endPoints
+    }
+  });
+}
+
+const registerCustomLandingPages = () => {
+  router.get("/:id", (req, res) => {
+    const id = req.params.id.toLowerCase();
+    const data = CustomEndpoints.filter(endpoint => endpoint.name === id)[0];
+
+    if (data) {
+      res.render("custom", {
+        ...data
+      });
+    } else {
+      res.render("404");
+    }
+  });
+}
+
+const registerCustomEndPoints = () => {
+  CustomEndpoints.forEach((endpoint) => {
+    const { name } = endpoint;
+    router.use(
+      `/${name}/api`,
+      apiLimits,
+      jsonServer.router(path.join(__dirname, `../custom/${name}.json`))
+    );
+
+    let data = getFromFile(path.join(__dirname, `../api/${link}.json`));
+    try {
+      router.use(
+        `/${link}/graphql`,
+        apiLimits, 
+        jsonGraphqlExpress.default(data)
+      );
+    } catch (err) {
+      console.log(`Unable to set up /${link}/graphql`);
+      console.error(err);
+    }
+  });
+}
+
+router.get("/create/:name", async (req, res) => {
+  const name = req.params.name.toLowerCase();
+  const files = await fs.readdirSync(directoryPath);
+  const exists = files.filter(file => {
+    const fileName = file.split('.')[0].toLowerCase();
+    return fileName == name;
+  })[0];
+
+  const baseData = {
+    info: { name }
+  }
+
+  if (!exists) {
+    fs.writeFileSync(
+      path.join(__dirname, `../custom/${name}.json`), 
+      JSON.stringify(baseData)
+    );
+    init();
+  }
+
+  res.redirect(`/custom/${name}`);
+})
+
 init();
-
-
-
 module.exports = router;
