ab51a87d7c7af95ed6773862e41d3f23761a9094
Jermbo <jermbo@cosmicstrawberry.com>
2020-10-08 16:29:41 -0400

    👌 IMPROVE: organizing utils

---
+++ src/utils/rateLimiterDefaults.js
@@ -0,0 +1,11 @@
+const rateLimit = require("express-rate-limit");
+
+const apiLimits = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 500,
+  message: "Too many requests, please try again after five minutes."
+});
+
+module.exports = {
+  apiLimits
+}
