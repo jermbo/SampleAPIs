c25120e66fca139d03752b6ecdcd859299a5ceb0
Jermbo <jermbo@cosmicstrawberry.com>
2020-11-20 14:19:16 -0500

    📦 NEW: Creating Endpoints for FrontEnd

--- server/routes/frontend.js
+++ server/routes/frontend.js
@@ -1,42 +1,53 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const router = express.Router();
 
 let APIList = [];
 const directoryPath = path.join(__dirname, "../api");
 
-router.get('/', async (req, res) => {
-  await getApiList();
+router.get("/", async (req, res) => {
+  if (!APIList.length) {
+    await getApiList();
+  }
 
   res.json({
     status: 200,
-    APIList
+    APIList,
+  });
+});
+
+router.get("/:name", async (req, res) => {
+  if (!APIList.length) {
+    await getApiList();
+  }
+
+  res.json({
+    data: 200,
+    id: req.params.name,
   });
 });
 
 const getApiList = async () => {
   let files = await fs.readdirSync(directoryPath);
-  files = files.filter(f => !f.includes('.backup'))
-  APIList = files.map(file => {
-    if (file.includes('.backup')) {
+  files = files.filter((f) => !f.includes(".backup"));
+  APIList = files.map((file) => {
+    if (file.includes(".backup")) {
       return false;
     }
-
-    console.log('file: ', file);
     const data = JSON.parse(fs.readFileSync(path.join(__dirname, `../api/${file}`)));
     const endpoints = Object.keys(data);
     const metaData = data.metaData[0];
-    const name = file.split('.')[0].toLowerCase();
+    const name = file.split(".")[0].toLowerCase();
 
     return {
       name,
       link: name,
       metaData,
-      endpoints: endpoints.filter(e => e != 'metaData'),
-    }
+      endpoints: endpoints.filter((e) => e != "metaData"),
+    };
   });
-}
+};
 
 module.exports = router;
