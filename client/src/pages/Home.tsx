9c5fa2771d84095bd12eaa31cc32d9de7a1625bf
Jermbo <jermbo@cosmicstrawberry.com>
2020-11-29 21:15:08 -0500

    👌 IMPROVE: colors

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
-
   return (
-    <>
-      <h1>Home Page</h1>
-    </>
-  )
-}
+    <section className="page">
+      <div className="page-header">
+        <h2 className="page__title">Sample APIs</h2>
+        <p className="page__sub-title">
+          A simple, no fuss, no mess, no auth, playground for learning <span>RESTful</span> or <span>GraphQL</span>{" "}
+          APIs.
+        </p>
+      </div>
+      <nav className="page-actions">
+        <p>
+          There is a lot to do with Sample APIs, why not
+          <Link className="text-btn" to="/about">
+            learn more about the project?
+          </Link>
+          We've got a huge list of data points
+          <Link className="text-btn" to="/api-list">
+            API List.
+          </Link>
+          Be sure to
+          <Link className="text-btn" to="/docs">
+            check out the docs
+          </Link>
+          for some guidance. Do you need a quick an simple api? Consider
+          <Link className="text-btn" to="/custom">
+            creating a custom endpoint.
+          </Link>
+        </p>
+      </nav>
+    </section>
+  );
+};
 
 export default Home;
