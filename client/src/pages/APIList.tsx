4f8185125976b91382069a1d7d8bfd5737d138a9
Jermbo <jermbo@cosmicstrawberry.com>
2020-12-04 20:15:56 -0500

    📦 NEW: Starting API List Page

--- client/src/pages/APIList.tsx
+++ client/src/pages/APIList.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import AllAPIs from "../components/AllAPIs";
 
 const APIList: React.FC = () => {
   return (
-    <>
-      <h1>APIList Page</h1>
-    </>
+    <section className="home-page">
+      <div className="home-page-header">
+        <h2 className="home-page__title">API List</h2>
+        <p className="home-page__sub-title">
+          Sample APIs has a growing list of endpoints. Perfect for any learning project
+        </p>
+      </div>
+      <AllAPIs />
+    </section>
   );
 };
 
