296a1f2b98f0858b6fd451433600c821afc479ce
Jermbo <jermbo@cosmicstrawberry.com>
2020-12-09 15:30:26 -0500

    👌 IMPROVE: Updating API Structure
    Removed the express static site section, as we are no longer serving the static pages
    
    Reordered the GraphQL code to come first and removed the need for the `/api` in the url.

--- server/routes/base-apis.js
+++ server/routes/base-apis.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const rateLimit = require("express-rate-limit");
 const jsonServer = require("json-server");
 const jsonGraphqlExpress = require("json-graphql-server");
 
@@ -11,39 +10,18 @@ const ApiList = require("../apiList");
 
 const router = express.Router();
 
-/// Main EndPoint Route
-router.get("/:id", (req, res) => {
-  const id = req.params.id.toLowerCase();
-  const data = ApiList.filter(api => id == api.link.toLowerCase())[0];
-
-  if (data) {
-    res.render("page", {
-      ...data
-    });
-  } else {
-    res.render("404");
-  }
-});
-
 // API EndPoint Route
 ApiList.forEach(({ link }) => {
-  router.use(
-    `/${link}/api`,
-    apiLimits,
-    jsonServer.router(path.join(__dirname, `../api/${link}.json`))
-  );
-
-  let data = getFromFile(path.join(__dirname, `../api/${link}.json`));
+  const dataPath = path.join(__dirname, `../api/${link}.json`);
+  const data = getFromFile(dataPath);
   try {
-    router.use(
-      `/${link}/graphql`,
-      apiLimits, 
-      jsonGraphqlExpress.default(data)
-    );
+    router.use(`/${link}/graphql`, apiLimits, jsonGraphqlExpress.default(data));
   } catch (err) {
     console.log(`Unable to set up /${link}/graphql`);
     console.error(err);
   }
+
+  router.use(`/${link}`, apiLimits, jsonServer.router(dataPath));
 });
 
 module.exports = router;
