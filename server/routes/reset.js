68f581d3097a1456ed5fa0a3e3ae3f09e446e936
jermbo <jermbo@users.noreply.github.com>
2021-09-09 08:44:44 -0400

    👌 IMPROVE: Trying to figure out how to trigger a server reload

--- server/routes/reset.js
+++ server/routes/reset.js
@@ -6,42 +6,46 @@ const ApiList = require("../apiList");
 
 // Reset API Route
 router.get("/all", (req, res) => {
-  ApiList.forEach(page => {
+  ApiList.forEach((page) => {
     const api = page.link;
     fs.copyFile(
       path.join(__dirname, `../api/${api}.json.backup`),
       path.join(__dirname, `../api/${api}.json`),
-      err => {
+      (err) => {
         if (err) {
           console.log(err);
         }
-      }
+      },
     );
   });
 
   res.render("api-reset", {
-    title: "Everything"
+    title: "Everything",
   });
+
+  // Need to figure out how to get this to exit and not cause an infinite reload loop
+  // process.exit(1);
 });
 
 // Main EndPoint Route
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   const id = req.params.id.toLowerCase();
-  const data = ApiList.filter(api => id == api.link.toLowerCase())[0];
+  const data = ApiList.filter((api) => id == api.link.toLowerCase())[0];
   const api = data.link;
-  fs.copyFile(
-    path.join(__dirname, `../api/${api}.json.backup`),
-    path.join(__dirname, `../api/${api}.json`),
-    err => {
-      if (err) {
-        console.log(err);
-      }
+  fs.copyFile(path.join(__dirname, `../api/${api}.json.backup`), path.join(__dirname, `../api/${api}.json`), (err) => {
+    if (err) {
+      console.log(err);
     }
-  );
+  });
 
   res.render("api-reset", {
-    ...data
+    ...data,
   });
+
+  res.redirect("/");
+
+  // Need to figure out how to get this to exit and not cause an infinite reload loop
+  // process.exit(1);
 });
 
 module.exports = router;
