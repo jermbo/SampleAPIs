7747c4455c36001a27d1157e7d9c06edeeed4af1
Damian Montero <photoca@hotmail.com>
2022-05-22 14:27:05 -0400

    endpoint checker script, maybe run in the future with commits, and update to the monstersanctuary backup file that wasn't equal

--- checkdiff.sh
+++ checkdiff.sh
@@ -1,134 +1,134 @@
 cd server/api
-DIFF=$(diff avatar.json avatar.json.backup}
+
+DIFF=$(diff avatar.json avatar.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "avatar modified"
 fi
-DIFF=$(diff baseball.json baseball.json.backup}
+DIFF=$(diff baseball.json baseball.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "baseball modified"
 fi
-DIFF=$(diff beers.json beers.json.backup}
+DIFF=$(diff beers.json beers.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "beers modified"
 fi
-DIFF=$(diff bitcoin.json bitcoin.json.backup}
+DIFF=$(diff bitcoin.json bitcoin.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "bitcoin modified"
 fi
-DIFF=$(diff cartoons.json cartoons.json.backup}
+DIFF=$(diff cartoons.json cartoons.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "cartoons modified"
 fi
-DIFF=$(diff codingresources.json codingresources.json.backup}
+DIFF=$(diff codingresources.json codingresources.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "codingresources modified"
 fi
-DIFF=$(diff coffee.json coffee.json.backup}
+DIFF=$(diff coffee.json coffee.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "coffee modified"
 fi
-DIFF=$(diff countries.json countries.json.backup}
+DIFF=$(diff countries.json countries.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "countries modified"
 fi
-DIFF=$(diff 
-csscolornames.json csscolornames.json.backup}
+DIFF=$(diff csscolornames.json csscolornames.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "csscolornames modified"
 fi
-DIFF=$(diff fakebank.json fakebank.json.backup}
+DIFF=$(diff fakebank.json fakebank.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "fakebank modified"
 fi
-DIFF=$(diff football.json football.json.backup}
+DIFF=$(diff football.json football.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "football modified"
 fi
-DIFF=$(diff futurama.json futurama.json.backup}
+DIFF=$(diff futurama.json futurama.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "futurama modified"
 fi
-DIFF=$(diff health.json health.json.backup}
+DIFF=$(diff health.json health.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "health modified"
 fi
-DIFF=$(diff jokes.json jokes.json.backup}
+DIFF=$(diff jokes.json jokes.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "jokes modified"
 fi
-DIFF=$(diff monstersanctuary.json monstersanctuary.json.backup}
+DIFF=$(diff monstersanctuary.json monstersanctuary.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "monstersanctuary modified"
 fi
-DIFF=$(diff movies.json movies.json.backup}
+DIFF=$(diff movies.json movies.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "movies modified"
 fi
-DIFF=$(diff playstation.json playstation.json.backup}
+DIFF=$(diff playstation.json playstation.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "playstation modified"
 fi
-DIFF=$(diff presidents.json presidents.json.backup}
+DIFF=$(diff presidents.json presidents.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "presidents modified"
 fi
-DIFF=$(diff recipes.json recipes.json.backup}
+DIFF=$(diff recipes.json recipes.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "recipes modified"
 fi
-DIFF=$(diff rickandmorty.json rickandmorty.json.backup}
+DIFF=$(diff rickandmorty.json rickandmorty.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "rickandmorty modified"
 fi
-DIFF=$(diff simpsons.json simpsons.json.backup}
+DIFF=$(diff simpsons.json simpsons.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "simpsons modified"
 fi
-DIFF=$(diff switch.json switch.json.backup}
+DIFF=$(diff switch.json switch.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "switch modified"
 fi
-DIFF=$(diff thestates.json thestates.json.backup}
+DIFF=$(diff thestates.json thestates.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "thestates modified"
 fi
-DIFF=$(diff typer.json typer.json.backup}
+DIFF=$(diff typer.json typer.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "typer modified"
 fi
-DIFF=$(diff wines.json wines.json.backup}
+DIFF=$(diff wines.json wines.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "wines modified"
 fi
-DIFF=$(diff xbox.json xbox.json.backup}
+DIFF=$(diff xbox.json xbox.json.backup)
 if [ "$DIFF" != "" ] 
 then
     echo "xbox modified"
 fi
-DIFF=$(diff 
 cd ../..
+
