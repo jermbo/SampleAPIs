c59f2933ddb2812c007e8769a483d35f58b026eb
Damian Montero <photoca@hotmail.com>
2022-08-08 16:19:30 -0400

    Adding the BocaCode bootcamp to the allow list

--- server/utils/rateLimiterDefaults.js
+++ server/utils/rateLimiterDefaults.js
@@ -1,10 +1,12 @@
 const rateLimit = require("express-rate-limit");
 
+const allowlist = ['170.55.81.98']
+
 const apiLimits = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
   max: 500,
   message: "Too many requests, please try again after five minutes.",
-  skip: (req,res) => { req.ip =="170.55.81.98" ? true : false }
+  skip: (req,res) =>  allowlist.includes(req.ip)
 });
 
 module.exports = {
