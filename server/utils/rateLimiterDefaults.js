dcc4285fa2e2a1223bed814ae2ca693225c119bd
Damian Montero <photoca@hotmail.com>
2022-12-05 20:18:29 +0000

    move time to hit the endpoint

--- server/utils/rateLimiterDefaults.js
+++ server/utils/rateLimiterDefaults.js
@@ -3,8 +3,8 @@ const rateLimit = require("express-rate-limit");
 const allowlist = ['170.55.81.98']
 
 const apiLimits = rateLimit({
-  windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 500,
+  windowMs: 15 * 60 * 1000, // 5 minutes
+  max: 5000,
   message: "Too many requests, please try again after five minutes.",
   skip: (req,res) =>  allowlist.includes(req.ip)
 });
