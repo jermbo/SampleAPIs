1174f952890a1921531edf23536b52b988d9cbb7
Damian Montero <photoca@hotmail.com>
2023-10-26 04:55:30 +0000

    removing node-fetch which is causing problems

--- server/routes/testApis.js
+++ server/routes/testApis.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const ApiList = require("../apiList");
-const fetch = require("node-fetch");
+//const fetch = require("node-fetch");
 const router = express.Router();
 
 /// Main EndPoint Route
