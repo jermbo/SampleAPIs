a561612332da76dc0e7199a207098b541a7fa5db
Damian Montero <photoca@hotmail.com>
2024-06-10 20:57:20 -0400

    making the page more adsense firnedly. Let's see if this helps

--- client/src/pages/Docs/Docs.tsx
+++ client/src/pages/Docs/Docs.tsx
@@ -1,17 +1,83 @@
 import React from "react";
 import PageHeaderActions from "../../components/PageHeaderActions/PageHeaderActions";
+import "./Docs.scss";
 
-interface Props {}
+
+interface Props { }
 
 const Docs: React.FC<Props> = () => {
   return (
-    <div className="page -docs">
+    <div className="page-docs">
       <header className="page-header">
-        <h2 className="page-header__title">Docs</h2>
-        <p className="page-header__desc">Coming Real Soon...</p>
-        <PageHeaderActions currentPage="docs" />
+        <h2 className="page-header__title">Documentation</h2>
+        <p className="page-header__desc">
+          <div className="section">
+            <h2>
+              Sample APIs has been given a major overhaul. Due to the fact Vercel.com has changed the way their servers work, we have had to rethink how the system was built. Over all, the changes are for the better. It is now easier than ever to contribute.
+            </h2>
+          </div>
+          <div className="section">
+            <h2>Purpose</h2>
+            <p>Understanding RESTful APIs is hard enough, even without including an authentication mechanism. The sole purpose of this repository is to play with RESTful endpoints and learn. We have a few endpoints that you can start playing around with right away! If you are not finding anything you are interested in, create your own endpoints and/or submit a pull request. Take a look at the <a href="https://github.com/jermbo/SampleAPIs/blob/master/CONTRIBUTING.md">CONTRIBUTING</a> for more information on how to get involved.</p>
+          </div>
+          <div className="section">
+            <h2> How to use the service</h2>
+            <p>Choose an endpoint, say "futurama", then choose what information you'd like, say "characters":
+              <pre>
+                fetch("https://api.sampleapis.com/futurama/characters") <br />
+                .then(resp =&gt; resp.json()) <br />
+                .then(data =&gt; console.log(data)); <br />
+              </pre>
+              Want to Search? for all chatacters with the name "Bender"?
+              <pre>
+                fetch(`https://api.sampleapis.com/futurama/characters?name.first=Bender`) <br />
+            .then(resp =&gt; resp.json()) <br />
+            .then(data =&gt; console.log(data)); <br />
+              </pre>
+              <p>
+
+            Want to learn more? Try watching this video that will explain how to use this api and showcase the results 
+            without any front end coding: <a href="https://www.youtube.com/watch?v=lCs9EriXnY8">https://www.youtube.com/watch?v=lCs9EriXnY8</a>
+
+              </p>
+            </p>
+          </div>
+          <div className="section">
+            <p>
+              You also have full CRUD, so you can add information or correct existing ones.
+              <br /> <br />
+              Note: Just know that we reset all datapoints weekly and each time we have a new endpoint added.
+            </p>
+          </div>
+          <div className="section">
+            <h2>Disclaimers</h2>
+            <p>
+              <ul>
+                <li>The data on this site is for educational purposes only and is not owned by SampleAPIs.com</li>
+                <li>Data will be reset back to its original state on a regular basis. If you are updating or adding data to the endpoints and want to have them persist as part of the collection, please contribute to the repo by submitting a pull request.</li>
+                <li>By using SampleAPIs.com you agree to the following terms: This service is provided under an "as is" condition. It might change or will be discontinued without prior notice. The maker of this service can't be held liable in any way for any reason.</li>
+              </ul>
+            </p>
+          </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </p>
+
       </header>
     </div>
+
   );
 };
 
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -11,16 +11,23 @@ interface Props {}
 const Home: React.FC<Props> = () => {
   const { apiList } = useContext(GlobalContext);
   const [featuredAPIs, setFeatureAPIs] = useState([] as APIData[]);
-
+  const [coffees, setCoffees] = useState([] as any[]);
+  
   useEffect(() => {
     const featured = apiList
       .filter((api) => api.metaData.featured)
       .sort(() => (Math.random() > 0.5 ? 1 : -1));
     setFeatureAPIs(featured);
+
+    fetch("https://api.sampleapis.com/coffee/hot")
+      .then((resp) => resp.json())
+      .then((data) => setCoffees(data));
+
+
   }, [apiList]);
 
   return (
-    <section className="page -home">
+    <section className="page-home">
       <header className="page-header">
         <h2 className="page-header__title">Sample APIs</h2>
         <p className="page-header__desc">
@@ -33,10 +40,39 @@ const Home: React.FC<Props> = () => {
       </header>
       <div className="section">
         <div className="section-header">
+          <h3 className="section-title">Example</h3>
+        </div>
+        <div className="section-desc">
+          <p>
+            Understanding RESTful APIs is hard enough, even without including an
+            authentication mechanism. The sole purpose of this repository is to
+            play with RESTful endpoints and learn. We have a few endpoints that
+            you can start playing around with right away!
+          </p>
+          <p> Here's an example where I've pulled from: 
+            and chosen the "hot" endpoint: https://api.sampleapis.com/coffee/hot<br />
+            then we ran the following code:
+            <pre>
+              fetch("https://api.sampleapis.com/coffee/hot") <br />
+              .then(resp =&gt; resp.json()) <br />
+              .then(data =&gt; console.log(data[0].title)); <br />
+            </pre>
+            and randomly selected a coffee and we get the following json back: 
+            <pre>
+              {JSON.stringify(coffees[0].title, null, 2)}
+            </pre>
+            Want to learn more? Try watching this video that will explain how to use this api and showcase the results 
+            without any front end coding: <a href="https://www.youtube.com/watch?v=lCs9EriXnY8">https://www.youtube.com/watch?v=lCs9EriXnY8</a>
+          </p>
+          </div>  
+      </div>
+      <div className="section">
+        <div className="section-header">
           <h3 className="section-title">
             Featured <abbr title="Application Program Interface">API</abbr>s
           </h3>
         </div>
+        
         <div className="cards-grid">
           {featuredAPIs &&
             featuredAPIs.map((api) => (
